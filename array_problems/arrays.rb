e5995962a8795b017a456f8ce3a9c6ca41f80950
Gabe Polk <gabe.a.polk@gmail.com>
2014-06-09 09:21:48 -0700

    Finished algorithms

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,11 +3,44 @@ require 'pry-debugger'
 
 module ArrayUtil
   def self.max(array)
+    sorted = false
+
+    until sorted
+      sorted = true
+      (array.count - 1).times do |i|
+        if array[i] > array[i + 1]
+          array[i], array[i + 1] = array[i + 1], array[i]
+          sorted = false
+        end
+      end
+    end
+
+    array[array.count - 1]
   end
 
   def self.middle_element(array)
+    if array.any? == false
+      return nil
+    elsif array.count == 1
+      return array[0]
+    elsif array.count % 2 != 0
+      return array[array.count / 2]
+    elsif array.count % 2 == 0
+      return (array[array.count / 2 - 1] + array[array.count / 2]) / 2.0
+    end
   end
 
   def self.sum_arrays(array1, array2)
+    if array1.any? && array2.any? == false
+      return []
+    else
+      new_array = []
+      i = 0
+      while i < array1.count
+        new_array << array1[i] + array2[i]
+        i += 1
+      end
+      return new_array
+    end
   end
 end
--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,66 @@
 module Set1
   def self.swap_small(array)
+    if array.count == 1
+      return array
+    elsif array.count == 2
+      return array.sort
+    elsif array.sort.first == array[0]
+      array
+    else
+      # O(n)
+      smallest = array.sort.first
+      # O(n)
+      array.each_with_index do |item, idx|
+        if item == smallest
+          # O(2)
+          array[0], array[idx] = array[idx], array[0]
+        end
+      end
+      # Total: O(2n^2)
+    end
   end
 
   def self.find_sum_2(array, sum = 0)
+    if array.count == 0
+      return false
+    elsif array.count == 1 && array.first == 0
+      return true
+    elsif array.include?(0)
+      return true
+    elsif array.count != 0
+      array.detect{|num| num < 1 && array.include?(num * -1)} != nil
+    else
+      # Total: O(n^2)
+      array.each do |num1|
+        array.each do |num2|
+          if num1 - num2 == 0 && num2 - num1 == 0
+            return false
+          end
+        end
+      end
+    end
   end
 
   def self.find_sum_3(array)
+    if array.count == 0
+      return false
+    elsif array.count == 1 && array[0] == 0
+      return true
+    elsif array.include?(0)
+      return true
+    elsif array.count != 0
+      # Total: O(n^3)
+      array.each do |num1|
+        array.each do |num2|
+          array.each do |num3|
+            if num1 + num2 + num3 == 0
+              return true
+            else
+              return false
+            end
+          end
+        end
+      end
+    end
   end
 end
